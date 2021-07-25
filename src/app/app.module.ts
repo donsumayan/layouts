3eea19b434656ae939016817d941cda72a6bf033
donsumayan <don.sumayan@gmail.com>
2021-07-25 19:21:50 +0800

    first ocmmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewUserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatSelectModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view-user/view-user.component.ts
@@ -0,0 +1,48 @@
+import { ViewportScroller } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-view-user',
+  templateUrl: './view-user.component.html',
+  styleUrls: ['./view-user.component.css']
+})
+export class ViewUserComponent {
+
+  generalForm = new FormGroup({
+    email: new FormControl(),
+    fname: new FormControl(),
+    lname: new FormControl(),
+    phone: new FormControl(),
+    companyName: new FormControl(),
+    title: new FormControl(),
+  });
+
+  securityForm = new FormGroup({
+    role: new FormControl('Transaction Manager'),
+    setSSO: new FormControl(false),
+    ssoIdentity: new FormControl(),
+  })
+
+  scrollElementId = 'general';
+
+  constructor(private scroller: ViewportScroller) {
+    this.scroller.setOffset([0, 184])
+    this.generalForm.disable();
+    this.securityForm.disable();
+  }
+
+
+  scrollTo(elementId: string) {
+    this.scroller.scrollToAnchor(elementId);
+    this.scrollElementId = elementId;
+  }
+
+  saveGeneralFormChanges() {
+    console.log(this.generalForm.value)
+  }
+
+  saveSecurityFormChanges() {
+    console.log(this.generalForm.value)
+  }
+}
